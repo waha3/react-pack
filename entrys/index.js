b1cfd64ab539b24c7293314fbe39b6f7e0aecb48
韩鑫 <hanxin@192.168.0.103>
2018-07-06 01:19:27 +0800

    add axios

--- entrys/index.js
+++ entrys/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import http from 'axios';
+
+http.get('https://cdnjs.cloudflare.com/ajax/libs/react-router/4.3.1/react-router.min.js');
 
 export default class Main extends Component {
   render() {
